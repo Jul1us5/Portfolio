46ec3dce1c6065a08d8b6898136f53a5a76cc0d4
Julius <Julius>
2020-08-24 22:52:26 +0300

    some fix

--- js/data.js
+++ js/data.js
@@ -54,3 +54,49 @@ const nav = [
         text: 'PS',
     }
 ];
+
+const links = [
+    {
+        link: '#',
+        title: 'Vilnius, Lietuva',
+        img: 'fa-map-marker',
+    },
+    {
+        link: '#',
+        title: 'Julius Vlasovas',
+        img: 'fa-user-circle-o',
+    },
+    {
+        link: '#',
+        title: 'FRONT-END',
+        img: 'fa-cube',
+    },
+    {
+        link: '#',
+        title: '+37064835040',
+        img: 'fa-phone',
+    },
+    {
+        link: 'https://www.linkedin.com/in/julius-vlasovas-3657611a3/',
+        title: 'LinkedIn',
+        img: 'fa-linkedin',
+    },
+    {
+        link: 'https://github.com/Jul1us5',
+        title: 'GIT',
+        img: 'fa-github',
+    } 
+];
+
+const texts = [
+    {
+        text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero nisi obcaecati fugit rem animi
+        sequi delectus cum quo deserunt inventore accusamus est, quis ipsum dignissimos quam quibusdam
+        culpa dolores veritatis molestiae. Adipisci m ratione tempora repellat necessitatibus
+        inventore, beatae voluptatem aliquid sed blanditiis, illo quae. Quod, non? Placeat iure unde
+        repudiandae voluptatum omnis tempore et necessitatibus laudantium?eveniet totam aliquid tempora id labore est facilis
+        veniam? Dolorum numquam laboriosam dolore, dolorem ratione tempora repellat necessitatibus
+        inventore, beatae voluptatem aliquid sed blanditiis, illo quae. Quod, non? Placeat iure unde
+        repudiandae voluptatum omnis tempore et necessitatibus laudantium?`
+    } 
+];
